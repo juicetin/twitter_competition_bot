89d65b415a862d4cc5b8ac94b699d8ab8baf461c
Justin Ting <justingling@gmail.com>
2016-02-09 00:23:16 +1100

    improved syntax in migrations

--- db/migrate/20160208124433_create_following_table.rb
+++ db/migrate/20160208124433_create_following_table.rb
@@ -1,7 +1,9 @@
 class CreateFollowingTable < ActiveRecord::Migration
   def change
-    create_table :following_tables do |t|
-		# Add id, name, date of follow
+    create_table :following do |t|
+		t.string 	:user_id
+		t.string 	:user_handle
+		t.datetime 	:date_created
     end
   end
 end
--- db/migrate/20160208124457_create_tweets_table.rb
+++ db/migrate/20160208124457_create_tweets_table.rb
@@ -1,8 +1,11 @@
 class CreateTweetsTable < ActiveRecord::Migration
   def change
-    create_table :tweets_tables do |t|
-		# Add id, text, tweeter, 
-		# 	*status* (i.e. retweeted, skipped, etc.)
+    create_table :tweets do |t|
+		t.string 	:tweet_id
+		t.string 	:tweet_user_id
+		t.string 	:tweet_text
+		t.string 	:status
+		t.datetime 	:date_created
     end
   end
 end
