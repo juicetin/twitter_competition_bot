d19eb9f4e5fe450fa8144df58d2df4816b745bac
Justin Ting <justingling@gmail.com>
2016-02-13 09:50:43 +1100

    restored skeleton for integration twitter follow tests

---
+++ tests/integration/js/twitter/follow.js
@@ -0,0 +1,5 @@
+describe("the interface to twitter API's follow functionality", function() {
+	it("should be able to follow a user given their id or screen name");
+
+	it("should be able to unfollow a user given their id or screen name");
+});
